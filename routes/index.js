01c7681afda0b68eef9e8de4dc706cc6bdf243b6
Rodrigo <fontesr2@gmail.com>
2024-12-06 16:00:58 -0300

    chore: manifest path, img path

--- routes/index.js
+++ routes/index.js
@@ -52,6 +52,14 @@ router.get('/redirect', (req, res, next) => {
   )
 })
 
+router.get('/manifest.json', (req, res, next) => {
+  res.sendFile(path.join('/Users/rfontes/Downloads', 'manifest.json'))
+})
+
+router.get('/img.png', (req, res, next) => {
+  res.sendFile(path.join('/Users/rfontes/Downloads', 'img.png'))
+})
+
 router.all('/*', (req, res, next) => {
   const { body, headers: reqHeaders, method, query, url: reqUrl } = req
 
